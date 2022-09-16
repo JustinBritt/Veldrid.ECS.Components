544f7398022af6b09d523112505b82df1f843a1f
Justin Britt <britt@uwindsor.ca>
2022-09-16 18:45:15 -0400

    Added extension method

---
+++ Veldrid.ECS.Components/Extensions/WorldExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Veldrid.ECS.Components.Extensions
+{
+    using System.Linq;
+
+    using DefaultEcs;
+
+    using Veldrid;
+    using Veldrid.ECS.Components.Structs;
+
+    public static class WorldExtensions
+    {
+        public static Viewport GetViewportLast(
+            this World world)
+        {
+            return world
+                .GetEntities()
+                .With<ViewportComponent>()
+                .AsEnumerable()
+                .Where(w => w.IsEnabled() && w.IsAlive)
+                .Select(w => w.Get<ViewportComponent>().Value)
+                .Last();
+        }
+    }
+}
